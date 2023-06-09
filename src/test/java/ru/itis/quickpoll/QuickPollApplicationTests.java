96312f2c1fb60ed68e45024c369ec54a7ec87ec3
Timerbulat <timerhelmut@gmail.com>
2023-06-09 12:34:13 +0300

    add project

---
+++ src/test/java/ru/itis/quickpoll/QuickPollApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.quickpoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickPollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
