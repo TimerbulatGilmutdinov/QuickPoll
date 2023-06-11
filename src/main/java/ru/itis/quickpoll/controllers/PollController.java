747c98723d8232f8e16a89af4956d63dce25ae41
Timerbulat <timerhelmut@gmail.com>
2023-06-11 22:56:05 +0300

    added Pole id verification

--- src/main/java/ru/itis/quickpoll/controllers/PollController.java
+++ src/main/java/ru/itis/quickpoll/controllers/PollController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.itis.quickpoll.exceptions.ResourceNotFoundException;
 import ru.itis.quickpoll.models.Option;
 import ru.itis.quickpoll.models.Poll;
 import ru.itis.quickpoll.repositories.PollRepository;
@@ -24,23 +25,30 @@ public class PollController {
         return new ResponseEntity<>(allPolls, HttpStatus.OK);
     }
 
+    protected Poll verifyPoll(Long pollId) throws ResourceNotFoundException {
+        Optional<Poll> poll = pollRepository.findById(pollId);
+        if(poll.isEmpty()) {
+            throw new ResourceNotFoundException("Poll with id " +
+                    pollId + " not found");
+        }
+        return poll.get();
+    }
+
     @GetMapping("/polls/{id}")
     public ResponseEntity<?> getPoll(@PathVariable Long id) {
-        Optional<Poll> poll = pollRepository.findById(id);
-        if (poll.isEmpty()) {
-            throw new IllegalArgumentException("Poll not found");
-        }
-        return new ResponseEntity<>(poll.get(), HttpStatus.OK);
+        return new ResponseEntity<>(verifyPoll(id), HttpStatus.OK);
     }
 
     @PutMapping("/polls/{id}")
     public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long id) {
-        Poll newPoll = pollRepository.save(poll);
+        verifyPoll(id);
+        pollRepository.save(poll);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/polls/{id}")
     public ResponseEntity<?> deletePoll(@PathVariable Long id) {
+        verifyPoll(id);
         pollRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
